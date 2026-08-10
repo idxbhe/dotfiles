40f29be9ba0920600d5c5ef3b10643d3a53d2b97
bhe <bhe@example.com>
2026-08-10 18:33:19 +0700

    feat: updating emilia & jan theme

--- config/bspwm/rices/emilia/theme-config.bash
+++ config/bspwm/rices/emilia/theme-config.bash
@@ -29,8 +29,8 @@ accent_color="#222330"
 arch_icon="#0f94d2"
 
 # Bspwm options
-BORDER_WIDTH="0"		# Bspwm border
-TOP_PADDING="50"
+BORDER_WIDTH="1"		# Bspwm border
+TOP_PADDING="25"
 BOTTOM_PADDING="1"
 LEFT_PADDING="1"
 RIGHT_PADDING="1"
@@ -42,19 +42,19 @@ term_font_size="10"
 term_font_name="JetBrainsMono Nerd Font"
 
 # Picom options
-P_FADE="true"			# Fade true|false
-P_SHADOWS="true"		# Shadows true|false
-SHADOW_C="#000000"		# Shadow color
-P_CORNER_R="6"			# Corner radius (0 = disabled)
-P_BLUR="false"			# Blur true|false
-P_ANIMATIONS="@"		# (@ = enable) (# = disable)
-P_TERM_OPACITY="1.0"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
+P_FADE="true"		# Fade true|false
+P_SHADOWS="true"	# Shadows true|false
+SHADOW_C="#000000"	# Shadow color
+P_CORNER_R="4"		# Corner radius (0 = disabled)
+P_BLUR="false"		# Blur true|false
+P_ANIMATIONS="@"	# (@ = enable) (# = disable)
+P_TERM_OPACITY="0.85"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
 
 # Dunst
 dunst_offset='(20, 60)'
 dunst_origin='top-right'
 dunst_transparency='0'
-dunst_corner_radius='6'
+dunst_corner_radius='3'
 dunst_font='JetBrainsMono NF Medium 9'
 dunst_border='0'
 dunst_frame_color="$accent_color"
--- config/bspwm/rices/jan/theme-config.bash
+++ config/bspwm/rices/jan/theme-config.bash
@@ -29,8 +29,8 @@ accent_color="#09021f"
 arch_icon="#0f94d2"
 
 # Bspwm options
-BORDER_WIDTH="0"		# Bspwm border
-TOP_PADDING="43"
+BORDER_WIDTH="1"		# Bspwm border
+TOP_PADDING="21"
 BOTTOM_PADDING="1"
 LEFT_PADDING="1"
 RIGHT_PADDING="1"
@@ -42,13 +42,13 @@ term_font_size="10"
 term_font_name="JetBrainsMono Nerd Font"
 
 # Picom options
-P_FADE="true"			# Fade true|false
-P_SHADOWS="false"		# Shadows true|false
-SHADOW_C="#000000"		# Shadow color
-P_CORNER_R="0"			# Corner radius (0 = disabled)
-P_BLUR="false"			# Blur true|false
-P_ANIMATIONS="@"		# (@ = enable) (# = disable)
-P_TERM_OPACITY="0.96"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
+P_FADE="true"		# Fade true|false
+P_SHADOWS="false"	# Shadows true|false
+SHADOW_C="#000000"	# Shadow color
+P_CORNER_R="3"		# Corner radius (0 = disabled)
+P_BLUR="false"		# Blur true|false
+P_ANIMATIONS="@"	# (@ = enable) (# = disable)
+P_TERM_OPACITY="0.9"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
 
 # Dunst
 dunst_offset='(20, 60)'
